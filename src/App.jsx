a3723e69e6c22d75ab34b100e3aadb78e8e7775c
likesbook <ikuno.ayaka+github@gmail.com>
2022-02-12 20:30:46 +0900

    マル罰ゲーム作った

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,101 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+import React from 'react'
+const Square = ({ children, onclick }) => {
+  return <button onClick={onclick} className="square">{children}</button>
 }
 
-export default App
+class Board extends React.Component {
+  state = { boardinfo: ['', '', '', '', '', '', '', '', '',] }
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[0]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[0] === 'maru' ? '丸' : ''}
+
+          </Square>
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[1]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[1] === 'maru' ? '丸' : ''}
+          </Square>
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[2]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[2] === 'maru' ? '丸' : ''}
+          </Square>
+        </div>
+        <div className="board-row">
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[3]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[3] === 'maru' ? '丸' : ''}
+          </Square>
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[4]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[4] === 'maru' ? '丸' : ''}
+          </Square>
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[5]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[5] === 'maru' ? '丸' : ''}
+          </Square>
+        </div>
+        <div className="board-row">
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[6]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[6] === 'maru' ? '丸' : ''}
+          </Square>
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[7]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[7] === 'maru' ? '丸' : ''}
+          </Square>
+          <Square onclick={() => { 
+            const arr = [...this.state.boardinfo]; 
+            arr[8]='maru'
+            this.setState({ boardinfo:arr }) }}>
+            {this.state.boardinfo[8] === 'maru' ? '丸' : ''}
+          </Square>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
